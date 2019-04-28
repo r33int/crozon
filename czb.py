94595752bbaa0dae7720df0aa22b6a7e8f5ced41
Enzo <me@r33.space>
2019-04-28 15:15:18 +0200

    Dirty scripts, to be improved

---
+++ czb.py
@@ -0,0 +1,86 @@
+import codecs
+
+enb = input('Enter eNb ID: ')
+print("eNb:", enb)
+
+tac = input('Enter TAC: ')
+print("TAC:", tac)
+
+lat = input('Enter lat: ')
+print("lat:", lat)
+
+lon = input('Enter lon: ')
+print("lon:", lon)
+
+pci = input('Enter s0 pci (cid 0,3,6,18,21): ')
+print("pci:", pci)
+pci1= int(pci)
+pci2= int(pci) + int(1)
+pci3= int(pci) + int(2)
+print (pci1)
+print (pci2)
+print (pci3)
+
+name = input('Enter name: ')
+print("name:", name)
+
+#lines to gen
+
+#1800
+line1= ("4G;208;20;0;{0};{1};{2};{3};{4};{5} [1800][S1];1850\n".format(tac, enb, pci1, lat, lon, name))
+line2= ("4G;208;20;1;{0};{1};{2};{3};{4};{5} [1800][S2];1850\n".format(tac, enb, pci2, lat, lon, name))
+line3= ("4G;208;20;2;{0};{1};{2};{3};{4};{5} [1800][S3];1850\n".format(tac, enb, pci3, lat, lon, name))
+
+#2600
+line4= ("4G;208;20;3;{0};{1};{2};{3};{4};{5} [2600][S1];3175\n".format(tac, enb, pci1, lat, lon, name))
+line5= ("4G;208;20;4;{0};{1};{2};{3};{4};{5} [2600][S2];3175\n".format(tac, enb, pci2, lat, lon, name))
+line6= ("4G;208;20;5;{0};{1};{2};{3};{4};{5} [2600][S3];3175\n".format(tac, enb, pci3, lat, lon, name))
+
+#800
+line7= ("4G;208;20;6;{0};{1};{2};{3};{4};{5} [800][S1];6200\n".format(tac, enb, pci1, lat, lon, name))
+line8= ("4G;208;20;7;{0};{1};{2};{3};{4};{5} [800][S2];6200\n".format(tac, enb, pci2, lat, lon, name))
+line9= ("4G;208;20;8;{0};{1};{2};{3};{4};{5} [800][S3];6200\n".format(tac, enb, pci3, lat, lon, name))
+
+#700
+line10= ("4G;208;20;18;{0};{1};{2};{3};{4};{5} [700][S1];9385\n".format(tac, enb, pci1, lat, lon, name))
+line11= ("4G;208;20;19;{0};{1};{2};{3};{4};{5} [700][S2];9385\n".format(tac, enb, pci2, lat, lon, name))
+line12= ("4G;208;20;20;{0};{1};{2};{3};{4};{5} [700][S3];9385\n".format(tac, enb, pci3, lat, lon, name))
+
+#2100
+line13= ("4G;208;20;21;{0};{1};{2};{3};{4};{5} [2100][S1];251\n".format(tac, enb, pci1, lat, lon, name))
+line14= ("4G;208;20;22;{0};{1};{2};{3};{4};{5} [2100][S2];251\n".format(tac, enb, pci2, lat, lon, name))
+line15= ("4G;208;20;23;{0};{1};{2};{3};{4};{5} [2100][S3];251\n".format(tac, enb, pci3, lat, lon, name))
+
+print(line1)
+print(line2)
+print(line3)
+print(line4)
+print(line5)
+print(line6)
+print(line7)
+print(line8)
+print(line9)
+print(line10)
+print(line11)
+print(line12)
+print(line13)
+print(line14)
+print(line15)
+
+f= codecs.open("bt.ntm","a+", "utf-8")
+f.write(line1)
+f.write(line2)
+f.write(line3)
+f.write(line4)
+f.write(line5)
+f.write(line6)
+f.write(line7)
+f.write(line8)
+f.write(line9)
+f.write(line10)
+f.write(line11)
+f.write(line12)
+f.write(line13)
+f.write(line14)
+f.write(line15)
+f.close()
